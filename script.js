74123f1da1afc57ebb0d5fc3b3d35d4e01ccd200
CC3636-IV <johansen.morten.m@gmail.com>
2024-09-06 12:14:05 +0200

    Light mode / Dark mode button finished

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function toggleTheme() {
     console.log("Theme button clicked")
+
+    let htmlElement = document.querySelector("html")
+    console.log(htmlElement.dataset.theme)
+
+    let currentTheme = htmlElement.dataset.theme
+
+    if (currentTheme === "dark") {
+        htmlElement.dataset.theme = "light"
+    } else {
+        htmlElement.dataset.theme = "dark"
+    }
 }
 
 let themebutton = document.getElementById("toggle-theme")
